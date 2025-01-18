bda1c57d2573f71b5a1811a2dc830e4cbe34741e
DeadliestOne <uncountableaura@gmail.com>
2025-01-19 02:14:13 +0530

    Update config.py

--- config.py
+++ config.py
@@ -1,6 +1,6 @@
 class Config:
-    API_ID = 
-    API_HASH = ""
-    TOKEN = ""  
-    START_PIC = "" 
-    CHAT = ""    
+    API_ID = 26416419
+    API_HASH = "c109c77f5823c847b1aeb7fbd4990cc4"
+    TOKEN = "7881162036:AAFqwmF2ny9TEMhNdbIohy7oh507PkWk5Wg"  
+    START_PIC = "https://files.catbox.moe/n6etx4.jpg" 
+    CHAT = "BeAkatsuki"    
